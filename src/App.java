82043f08994445c0ff65f95a0ee265d1bdda8372
Marcus Jarlevid <70881791+Jallelainen@users.noreply.github.com>
2021-11-19 12:37:13 +0100

    added method to check answer

--- src/App.java
+++ src/App.java
@@ -1,29 +1,59 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        
         System.out.println("Hello, World! I am live. How are you feeling today?");
 
-        String[] positiveStr = {"good", "fine", "wonderful", "happy", "great", "fantastic"};
-        String[] negativeStr = {"bad", "not so good", "horrible", "im feeling down", "sad"};
-        BufferedReader obj = new BufferedReader(new InputStreamReader(System.in));
-        String str;
+        Scanner userInput = new Scanner(System.in);
+        String str = userInput.nextLine().toLowerCase();
+        boolean keepLoop = true;
+        int sKey = handleInput(str);
 
-        str = obj.readLine().toLowerCase();
+            do {
+                switch (sKey) {
+                    case 1:
+                        System.out.println("I'm sorry to hear that...");
+                        keepLoop = false;
+                        userInput.close();
+                        break;
+                    case 2:
+                        System.out.println("Great! I hope your day continues that way. =)");
+                        keepLoop = false;
+                        userInput.close();
+                        break;
+                    case 3:
+                        System.out.println("Hmm.. I didnt seem to understand your answer. Please try typing another synonym for your answer.");
+                        sKey = handleInput(userInput.nextLine().toLowerCase());
+                        break;
+                }
+            } while (keepLoop);
 
-        for (int i = 0; i < negativeStr.length; i++) {
-            if (str.equals(negativeStr[i])) {
-            System.out.println("I'm sorry to hear that...");
-        }
+        };
+        
+        public static int handleInput(String input) {
+            String[] positiveStr = {"good", "fine", "wonderful", "happy", "great", "fantastic"};
+            String[] negativeStr = {"bad", "not so good", "horrible", "im feeling down", "sad"};
+            int answer  = 3;
+
+            for (int i = 0; i < negativeStr.length; i++) {
+                if (input.equals(negativeStr[i])) {
+                    answer = 1; //negative answer
+                    break;
+                }else{
+                    for (int y = 0; y < positiveStr.length; y++) {
+                        if (input.equals(positiveStr[y])) {
+                            answer = 2;// positive answer
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return answer;
         }
         
-        for (int i = 0; i < positiveStr.length; i++) {
-            if (str.equals(positiveStr[i])) {
-            System.out.println("Great! I hope your day continues that way. =)");
-        }
-        }
     }
 
 
-}
+
